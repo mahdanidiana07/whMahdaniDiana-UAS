d2500b1235e8ad09781111973273db43f4350612
mahdanidiana07 <dianafitrriii21@gmail.com>
2025-05-17 01:38:41 -1200

    uas selesai

--- lib/data_saya.dart
+++ lib/data_saya.dart
@@ -1,4 +1,4 @@
 class DataSaya {
    static String nama = "Mahdani Diana";
-    static String gambar = "assets/mahasiswa/mahdani.jpeg";
+    static String gambar = "assets/mahasiswa/mahdanii.jpeg";
 }
